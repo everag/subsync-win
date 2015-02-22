54006f0b79f6511de4b4a43025ee8be30b2ce91c
evertonagner_cp <SND\evertonagner_cp>
2015-02-22 01:16:34 +0000

    Prevent multiple program instances

--- SubSync/Program.cs
+++ SubSync/Program.cs
@@ -1,7 +1,9 @@
 ﻿using SubSync.GUI;
+using SubSync.Lib;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -9,11 +11,43 @@ namespace SubSync
 {
     static class Program
     {
+        static Mutex mutex = new Mutex(true, "{DD54655E-8A62-4304-A57E-1594E302A0EE}");
+
         [STAThread]
         static void Main(string[] args)
         {
             StartupArgs.SetArgs(args);
 
+            if (Configuration.RunningEnvironment == "Debug")
+                StartApplicationInDebug();
+            else
+                StartApplication();
+        }
+
+        private static void StartApplication()
+        {
+            // Prevent multiple executions of application
+
+            if (mutex.WaitOne(TimeSpan.Zero, true))
+            {
+                InitializeGUI();
+                mutex.ReleaseMutex();
+            }
+            else
+            {
+                MessageBox.Show("SubSync is already running!", "SubSync");
+            }
+        }
+
+        private static void StartApplicationInDebug()
+        {
+            // Initialize application regardless
+
+            InitializeGUI();
+        }
+
+        private static void InitializeGUI()
+        {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new SetupForm());
