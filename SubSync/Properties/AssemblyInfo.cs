5c11da48b8b7efe108f4759e303e3786fea1faa9
evertonagner_cp <SND\evertonagner_cp>
2015-02-24 00:41:55 +0000

    Updated Assembly information

--- SubSync/Properties/AssemblyInfo.cs
+++ SubSync/Properties/AssemblyInfo.cs
@@ -6,13 +6,10 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SubSync")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyDescription("SubSync is your media subtitles hunter")]
+[assembly: AssemblyCompany("SubSync Team")]
 [assembly: AssemblyProduct("SubSync")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyCopyright("Copyleft 2015")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -32,5 +29,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.7.0.0")]
+[assembly: AssemblyInformationalVersion("SubSync Beta 0.7")]
