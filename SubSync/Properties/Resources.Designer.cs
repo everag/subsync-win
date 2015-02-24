1bbd5e287fc6708a0f176aab3b025b151dacea37
evertonagner_cp <SND\evertonagner_cp>
2015-02-24 00:42:34 +0000

    Updated Version information

--- SubSync/Properties/Resources.Designer.cs
+++ SubSync/Properties/Resources.Designer.cs
@@ -61,7 +61,7 @@ namespace SubSync.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to 150222.
+        ///   Looks up a localized string similar to 150223.
         /// </summary>
         internal static string AppBuildDate {
             get {
